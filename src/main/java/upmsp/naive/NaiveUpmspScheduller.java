566d989e287063120943c922441125d54f67efc6
Carlos G. Gavidia <cgavidiac@gmail.com>
2019-04-11 00:14:53 +0100

    Merge branch 'master' of E:\git\aco-upmsp with conflicts.

---
+++ src/main/java/upmsp/naive/NaiveUpmspScheduller.java
@@ -0,0 +1,13 @@
+package upmsp.naive;
+
+import upmsp.isula.sample.ProblemConfiguration;
+
+public class NaiveUpmspScheduller {
+
+    public static void main(String ... args) {
+
+        double[][] problemRepresentation = getProblemFromFile(ProblemConfiguration.INPUT_FILE,
+                ProblemConfiguration.SHEET_INDEX);
+
+    }
+}
