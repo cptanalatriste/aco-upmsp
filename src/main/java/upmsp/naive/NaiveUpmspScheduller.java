6112d7654bce6f1249894fb39ebfae2defcaeda1
Carlos G. Gavidia <cgavidiac@gmail.com>
2019-04-11 00:26:27 +0100

    Starting my naive scheduller

--- src/main/java/upmsp/naive/NaiveUpmspScheduller.java
+++ src/main/java/upmsp/naive/NaiveUpmspScheduller.java
@@ -1,13 +1,37 @@
 package upmsp.naive;
 
-import upmsp.isula.sample.ProblemConfiguration;
+import isula.aco.exception.InvalidInputException;
+import isula.aco.upmsp.UpmspEnvironment;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+import java.io.IOException;
+
+import static upmsp.isula.sample.AcoUpmspWithIsula.getProblemFromFile;
+import static upmsp.isula.sample.UpmspProblemConfiguration.INPUT_FILE;
+import static upmsp.isula.sample.UpmspProblemConfiguration.SHEET_INDEX;
 
 public class NaiveUpmspScheduller {
 
-    public static void main(String ... args) {
+    private UpmspEnvironment environment = null;
 
-        double[][] problemRepresentation = getProblemFromFile(ProblemConfiguration.INPUT_FILE,
-                ProblemConfiguration.SHEET_INDEX);
+    public NaiveUpmspScheduller(double[][] problemRepresentation) throws InvalidInputException {
+        this.environment = new UpmspEnvironment(problemRepresentation);
+    }
+
+
+    public static void main(String... args) {
+
+        try {
+            double[][] problemRepresentation = getProblemFromFile(INPUT_FILE,
+                    SHEET_INDEX);
+            NaiveUpmspScheduller naiveScheduller = new NaiveUpmspScheduller(problemRepresentation);
+        } catch (InvalidInputException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
